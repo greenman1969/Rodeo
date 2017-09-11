d665689e994aa1c45357836f48865c035e907802
Gage Greenhouse <gsggage@yahoo.com>
2017-09-11 14:32:08 -0500

    Just a test

--- Rodeo.java
+++ Rodeo.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.TreeMap;
+import java.io.*;
+import java.util.*;
 public class Rodeo implements java.io.Serializable
 {
 	private TreeMap<Integer,Contestant> contestants;
@@ -12,5 +12,21 @@ public class Rodeo implements java.io.Serializable
 	public Rodeo()
 	{
 		
+	}
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		boolean i = true;
+		while(i)
+		{
+			System.out.print("$ ");
+			String s=input.nextLine();
+			if(s=="add")
+				System.out.println("Add a new Event");
+			else if(s=="delete")
+				System.out.println("Delete an Event");
+			else
+				System.out.println(s+" is not a valid command");
+		}
 	}
 }
